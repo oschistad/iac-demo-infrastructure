bf380286e5d056ee71838757d40df880ff1d49f7
OleAndréSchistad <oas@aztek.no>
2021-01-27 21:37:52 +0100

    Add credentials to AzureRM provider

--- providers.tf
+++ providers.tf
@@ -10,5 +10,17 @@ terraform {
 }
 
 provider "azurerm" {
+  # We recommend pinning to the specific version of the Azure Provider you're using
+  # since new versions are released frequently
+
   features {}
+
+  # More information on the authentication methods supported by
+  # the AzureRM Provider can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
 }
