b92684bf617d0da499c865d9334672ae5f98a86b
OleAndréSchistad <oas@aztek.no>
2021-01-28 17:08:06 +0100

    Remove impossible outputs

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,9 @@
 output "privatekey" {
   value = tls_private_key.adminuser.private_key_pem
 }
-output "ip" {
-  value = azurerm_linux_virtual_machine.nomad_server.public_ip_addresses
-}
-output "ip2" {
-  value = azurerm_public_ip.public_ip.ip_address
-}
+//output "ip" {
+//  value = azurerm_linux_virtual_machine.nomad_server.public_ip_addresses
+//}
+//output "ip2" {
+//  value = azurerm_public_ip.public_ip.ip_address
+//}
